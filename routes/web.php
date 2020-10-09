f94442e9c9dd95b6e65b5995ce7fe3bc0f4d7823
Hanavi Alvarel <hanavialvarel@gmail.com>
2020-10-09 22:26:23 +0700

    Route master get by kategori

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['prefix' => 'master'], function () {
     Route::get('/','MasterController@index')->name('master.index');
     Route::get('/create','MasterController@create')->name('master.create');
     Route::get('/{master:slug}','MasterController@show')->name('master.show');
+    Route::get('/kategori/{kategori:slug}','MasterController@showByCategory')->name('master.showByCategory');
     Route::get('/{master:slug}/edit','MasterController@edit')->name('master.edit');
     Route::post('/store','MasterController@store')->name('master.store');
     Route::put('/{master:slug}/edit','MasterController@update')->name('master.update');
