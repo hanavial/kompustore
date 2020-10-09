45c346b66cf17f5a5f5f0a2b8bd7b9f4d20aaa2f
Hanavi Alvarel <hanavialvarel@gmail.com>
2020-10-09 22:29:13 +0700

    Migration kategori add slug

--- database/migrations/2020_07_14_170837_create_kategori_table.php
+++ database/migrations/2020_07_14_170837_create_kategori_table.php
@@ -16,7 +16,7 @@ class CreateKategoriTable extends Migration
         Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
             $table->string('kategori_nama');
-            $table->string('slug');
+            $table->string('slug'); 
             $table->timestamps();
         });
     }
